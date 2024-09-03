e5ba61d2dad0d3f3cf81b4a1351bc4f4523ab57e
bhnone <>
2024-09-03 23:01:49 +0900

    input components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import {Button} from "@/components/ui/button";
+import {Input} from "@/components/ui/input";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
           <code className="font-mono font-bold">src/app/page.tsx</code>
         </p>
         <Button className="p-10" variant={"outline"}>Click to buy</Button>
+        <Input type={"text"} name={"firstName"}/>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
